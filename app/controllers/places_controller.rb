6a44f5b7f33a3f840753337c6f764f5320101deb
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-08 16:24:53 +0000

    change 5

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,7 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @user = User.all
   end
 
   def show
@@ -15,6 +16,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
+    @place["id"] = Place.where({ "user_id" => @user["id"]})
     @place.save
     redirect_to "/places"
   end
