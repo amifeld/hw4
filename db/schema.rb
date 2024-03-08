6e59cb45b1c655ce3ab22d51023d2d85a2386176
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-08 00:47:09 +0000

    change 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_001153) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "places", "users"
 end
