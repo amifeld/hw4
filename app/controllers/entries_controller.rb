3aea2d140f40e264e91494e3b1d9ef3a2ea36c17
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-08 19:07:37 +0000

    changes 12

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,15 @@
 class EntriesController < ApplicationController
+  def index
+    @posts = Post.all
+
+    # alternative responses to requests other than HTML
+    respond_to do |format|
+      format.html # implicitly renders posts/index.html.erb
+      format.json do
+        render :json => @posts
+      end
+    end
+  end
 
   def new
   end
@@ -8,6 +19,7 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    # @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = @current_user["id"]
     @entry.save
@@ -15,3 +27,11 @@ class EntriesController < ApplicationController
   end
 
 end
+
+# before_action :allow_cors
+# def allow_cors
+#   response.headers['Access-Control-Allow-Origin'] = '*'
+#   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+#   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+#   response.headers['Access-Control-Max-Age'] = '1728000'
+# end
