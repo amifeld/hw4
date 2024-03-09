36603c8712c24a7e31c7d60228f45bb8d3289873
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-09 23:28:34 +0000

    dont forget

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,12 @@
 class EntriesController < ApplicationController
   def index
-    @posts = Post.all
+    @entry = Entry.all
 
     # alternative responses to requests other than HTML
     respond_to do |format|
       format.html # implicitly renders posts/index.html.erb
       format.json do
-        render :json => @posts
+        render :json => @entry
       end
     end
   end
@@ -24,15 +24,18 @@ class EntriesController < ApplicationController
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = @current_user["id"]
     @entry.save
+    
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
+
+
+before_action :allow_cors
+def allow_cors
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+  response.headers['Access-Control-Max-Age'] = '1728000'
 end
 
-# before_action :allow_cors
-# def allow_cors
-#   response.headers['Access-Control-Allow-Origin'] = '*'
-#   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-#   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-#   response.headers['Access-Control-Max-Age'] = '1728000'
-# end
+end
