01af08032f26c16a8a343e3e071f6007b65947c0
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-10 22:40:24 +0000

    change 205

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,11 @@ class SessionsController < ApplicationController
         flash["notice"] = "Welcome."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
+        flash["notice"] = "Incorrect email or password."
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Nope."
+      flash["notice"] = "Incorrect email or password."
       redirect_to "/login"
     end
   end
