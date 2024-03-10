95b94a0a57c294c6ca7523a447209aebff955b90
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-10 00:14:07 +0000

    bbb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_09_230532) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_09_233253) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "description"
