5402d085d6ffc37bce3425658794668e2a898770
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-03-10 22:34:14 +0000

    change 203

--- db/migrate/20240308001153_add_user_id_to_places.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToPlaces < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :places, :user, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_001656) do
     t.string "title"
     t.text "description"
     t.date "occurred_on"
+    t.string "image"
     t.integer "place_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -51,10 +52,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_001656) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,5 +67,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_001656) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "places", "users"
 end
